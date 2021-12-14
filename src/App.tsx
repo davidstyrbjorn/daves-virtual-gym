a4604bacc0ee33cef21648e16325f4b42d984eb3
davidstyrbjorn <davidstyrbjorn@gmail.com>
2021-12-14 08:09:18 +0100

    Navigation basics

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    
+    <Text style={styles.text}>
+      Dave's Virtual Gym
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text:{
+    fontFamily: "monospace",
+    textAlign: 'center',
+    paddingTop: 30,
+    color: "#160F29"
+  }
+});
+
+export default App;
